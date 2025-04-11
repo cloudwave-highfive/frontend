35cdae2543a302173ba546d322a22b60789f4c15
feel1toa <ssongsams@naver.com>
2025-04-11 09:03:03 +0900

    feat: 페이지 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import OrderCompletePage from './pages/OrderCompletePage';
+import ProductDetailPage from './pages/ProductDetailPage';
+import PaymentPage from './pages/PaymentPage';
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/order/complete" element={<OrderCompletePage />} />
+        <Route path="/product" element={<ProductDetailPage />} />
+        <Route path='/order' element={<PaymentPage />} />
       </Routes>
     </BrowserRouter>
   );
